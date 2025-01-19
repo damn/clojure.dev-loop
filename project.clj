eb919fe539976ac3093139312fea7ff3ee066065
Michael Sappler <michaelsappler@gmail.com>
2025-01-19 13:45:02 +0100

    clojure.dev-loop

--- project.clj
+++ project.clj
@@ -1,9 +1,6 @@
-(defproject clojure.gdx.dev-loop "-SNAPSHOT"
+(defproject clojure.dev-loop "-SNAPSHOT"
   :repositories [["jitpack" "https://jitpack.io"]]
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clj-commons/pretty "3.2.0"]
                  [nrepl "0.9.0"]
-                 [org.clojure/tools.namespace "1.3.0"]]
-  :plugins [[lein-codox "0.10.8"]]
-  :codox {:source-uri "https://github.com/damn/moon/blob/main/{filepath}#L{line}"
-          :metadata {:doc/format :markdown}})
+                 [org.clojure/tools.namespace "1.3.0"]])
